9e4941b57255b9847963cdcabbf0b8caab160669
jovanzac <jovanzac@gmail.com>
2023-06-06 23:32:54 +0530

    Added a page

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,17 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 0, 0, 153),
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            "ACTIVV",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 76,
+              fontFamily: "Barlow-Bold",
+              letterSpacing: 5
+            ),
+          ),
         ),
       ),
     );
