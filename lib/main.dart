986aa0b87373b4fd25f57c0d75ad3f2b855817d6
Twentyquid <negativeradiations@gmail.com>
2023-06-11 18:19:57 +0530

    minor modification

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: FormPage(),
-      ),
-    );
+    return const MaterialApp(home: FormPage());
   }
 }
--- lib/widgets/login_pg.dart
+++ lib/widgets/login_pg.dart
@@ -1,5 +1,3 @@
-// import 'dart:js';
-
 import 'package:flutter/material.dart';
 
 final _formKey = GlobalKey<FormState>();
