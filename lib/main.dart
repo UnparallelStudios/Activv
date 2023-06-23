c63fafafa1aa89f52e17c51c551c407209014732
Twentyquid <negativeradiations@gmail.com>
2023-06-23 22:07:45 +0530

    built intial routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:activv/widgets/logo_pg.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello World!"),
-          ),
-        ),
-      );
+    return MaterialApp(
+      routes: {'/': (context) => const Logo()},
+    );
   }
 }
