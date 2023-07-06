8829826aded7c93467f31e937b4cd3cedba60c0c
Twentyquid <negativeradiations@gmail.com>
2023-07-06 20:54:34 +0530

    modified navbar changed charts

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:activv/widgets/chart.dart';
 import 'package:flutter/material.dart';
 import 'package:badges/badges.dart' as badges;
 
@@ -6,12 +7,36 @@ void main() => runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
     ));
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.business), label: "Business"),
+            BottomNavigationBarItem(icon: Icon(Icons.school), label: "School")
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: const Color.fromRGBO(0, 0, 153, 1),
+          onTap: _onItemTapped,
+        ),
         appBar: AppBar(
           title: const Padding(
             padding: EdgeInsets.fromLTRB(0.0, 15.0, 0, 0),
@@ -107,98 +132,114 @@ class Dashboard extends StatelessWidget {
               child: Container(
                 padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
                 color: const Color.fromRGBO(216, 216, 255, 1),
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      child: card,
+                    ),
+                    Container(
+                      child: card,
+                    ),
+                    Container(
+                      child: card,
+                    ),
+                    Container(
+                      child: card,
+                    )
+                  ],
+                ),
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                  padding: const EdgeInsets.fromLTRB(10.0, 8.5, 10.0, 10.0),
-                  color: const Color.fromRGBO(240, 240, 240, 1),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Column(
-                        children: [
-                          TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              foregroundColor:
-                                  const Color.fromARGB(255, 200, 152, 245),
-                              shape: const StadiumBorder(),
-                            ),
-                            child: const Icon(
-                              IconData(0xe105, fontFamily: 'MaterialIcons'),
-                              color: Colors.black,
-                              size: 17,
-                            ),
-                          ),
-                          const Text(
-                            'Label',
-                            style: TextStyle(
-                              color: Colors.black,
-                              height: 0.7,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                                foregroundColor:
-                                    const Color.fromARGB(255, 200, 152, 245),
-                                shape: const StadiumBorder()),
-                            child: const Icon(
-                              IconData(0xe105, fontFamily: 'MaterialIcons'),
-                              color: Colors.black,
-                              size: 17,
-                            ),
-                          ),
-                          const Text(
-                            'Label',
-                            style: TextStyle(
-                              color: Colors.black,
-                              height: 0.7,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                                foregroundColor:
-                                    const Color.fromARGB(255, 200, 152, 245),
-                                shape: const StadiumBorder()),
-                            child: badges.Badge(
-                              badgeContent: const SizedBox(
-                                  child: Center(
-                                child: Text('3',
-                                    style: TextStyle(
-                                        fontSize: 11, color: Colors.white)),
-                              )),
-                              position: badges.BadgePosition.topEnd(),
-                              child: const Icon(
-                                IconData(0xe105, fontFamily: 'MaterialIcons'),
-                                color: Colors.black,
-                                size: 17,
-                              ),
-                            ),
-                          ),
-                          const Text(
-                            'Label',
-                            style: TextStyle(
-                              color: Colors.black,
-                              height: 0.7,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  )),
-            )
+            // Expanded(
+            //   flex: 1,
+            //   child: Container(
+            //       padding: const EdgeInsets.fromLTRB(10.0, 8.5, 10.0, 10.0),
+            //       color: const Color.fromRGBO(240, 240, 240, 1),
+            //       child: Row(
+            //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //         children: <Widget>[
+            //           Column(
+            //             children: [
+            //               TextButton(
+            //                 onPressed: () {},
+            //                 style: TextButton.styleFrom(
+            //                   foregroundColor:
+            //                       const Color.fromARGB(255, 200, 152, 245),
+            //                   shape: const StadiumBorder(),
+            //                 ),
+            //                 child: const Icon(
+            //                   IconData(0xe105, fontFamily: 'MaterialIcons'),
+            //                   color: Colors.black,
+            //                   size: 17,
+            //                 ),
+            //               ),
+            //               const Text(
+            //                 'Label',
+            //                 style: TextStyle(
+            //                   color: Colors.black,
+            //                   height: 0.7,
+            //                 ),
+            //               ),
+            //             ],
+            //           ),
+            //           Column(
+            //             children: [
+            //               TextButton(
+            //                 onPressed: () {},
+            //                 style: TextButton.styleFrom(
+            //                     foregroundColor:
+            //                         const Color.fromARGB(255, 200, 152, 245),
+            //                     shape: const StadiumBorder()),
+            //                 child: const Icon(
+            //                   IconData(0xe105, fontFamily: 'MaterialIcons'),
+            //                   color: Colors.black,
+            //                   size: 17,
+            //                 ),
+            //               ),
+            //               const Text(
+            //                 'Label',
+            //                 style: TextStyle(
+            //                   color: Colors.black,
+            //                   height: 0.7,
+            //                 ),
+            //               ),
+            //             ],
+            //           ),
+            //           Column(
+            //             children: [
+            //               TextButton(
+            //                 onPressed: () {},
+            //                 style: TextButton.styleFrom(
+            //                     foregroundColor:
+            //                         const Color.fromARGB(255, 200, 152, 245),
+            //                     shape: const StadiumBorder()),
+            //                 child: badges.Badge(
+            //                   badgeContent: const SizedBox(
+            //                       child: Center(
+            //                     child: Text('3',
+            //                         style: TextStyle(
+            //                             fontSize: 11, color: Colors.white)),
+            //                   )),
+            //                   position: badges.BadgePosition.topEnd(),
+            //                   child: const Icon(
+            //                     IconData(0xe105, fontFamily: 'MaterialIcons'),
+            //                     color: Colors.black,
+            //                     size: 17,
+            //                   ),
+            //                 ),
+            //               ),
+            //               const Text(
+            //                 'Label',
+            //                 style: TextStyle(
+            //                   color: Colors.black,
+            //                   height: 0.7,
+            //                 ),
+            //               ),
+            //             ],
+            //           ),
+            //         ],
+            //       )),
+            // )
           ],
         ));
   }
--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,6 @@
 // import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
+// import 'package:syncfusion_flutter_charts/charts.dart';
 // import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 void main() {
@@ -27,16 +27,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Widget card = Expanded(
-    child: Padding(
-  padding: const EdgeInsets.symmetric(horizontal: 10),
+Widget card = const Padding(
+  padding: EdgeInsets.symmetric(horizontal: 10),
   child: Card(
     child: Padding(
-      padding: const EdgeInsets.fromLTRB(16, 16, 0, 16),
+      padding: EdgeInsets.fromLTRB(16, 16, 0, 16),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Column(
+          Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
@@ -50,67 +49,67 @@ Widget card = Expanded(
               )
             ],
           ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    SizedBox(
-                      height: 200,
-                      width: 200,
-                      child: SfCircularChart(
-                        palette: const [
-                          Color.fromARGB(255, 253, 216, 111),
-                        ],
-                        series: <CircularSeries>[
-                          DoughnutSeries<GDPData, String>(
-                              innerRadius: '60%',
-                              dataSource: getChartData(),
-                              xValueMapper: (GDPData data, _) => data.continent,
-                              yValueMapper: (GDPData data, _) => data.gdp)
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 200,
-                      width: 200,
-                      child: SfCircularChart(
-                        palette: const [
-                          Color.fromARGB(255, 253, 216, 111),
-                          Color.fromARGB(255, 143, 133, 255)
-                        ],
-                        series: <CircularSeries>[
-                          DoughnutSeries<GDPData, String>(
-                              innerRadius: "60%",
-                              cornerStyle: CornerStyle.bothCurve,
-                              dataSource: getChartData(),
-                              xValueMapper: (GDPData data, _) => data.continent,
-                              yValueMapper: (GDPData data, _) => data.gdp)
-                        ],
-                      ),
-                    ),
-                    const Column(
-                      children: [
-                        Text("total"),
-                        Text(
-                          "50%",
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.w600),
-                        )
-                      ],
-                    ),
-                  ],
-                )
-              ],
-            ),
-          )
+          // Expanded(
+          //   child: Column(
+          //     crossAxisAlignment: CrossAxisAlignment.center,
+          //     children: [
+          //       Stack(
+          //         alignment: Alignment.center,
+          //         children: [
+          //           SizedBox(
+          //             height: 200,
+          //             width: 200,
+          //             child: SfCircularChart(
+          //               palette: const [
+          //                 Color.fromARGB(255, 253, 216, 111),
+          //               ],
+          //               series: <CircularSeries>[
+          //                 DoughnutSeries<GDPData, String>(
+          //                     innerRadius: '60%',
+          //                     dataSource: getChartData(),
+          //                     xValueMapper: (GDPData data, _) => data.continent,
+          //                     yValueMapper: (GDPData data, _) => data.gdp)
+          //               ],
+          //             ),
+          //           ),
+          //           SizedBox(
+          //             height: 200,
+          //             width: 200,
+          //             child: SfCircularChart(
+          //               palette: const [
+          //                 Color.fromARGB(255, 253, 216, 111),
+          //                 Color.fromARGB(255, 143, 133, 255)
+          //               ],
+          //               series: <CircularSeries>[
+          //                 DoughnutSeries<GDPData, String>(
+          //                     innerRadius: "60%",
+          //                     cornerStyle: CornerStyle.bothCurve,
+          //                     dataSource: getChartData(),
+          //                     xValueMapper: (GDPData data, _) => data.continent,
+          //                     yValueMapper: (GDPData data, _) => data.gdp)
+          //               ],
+          //             ),
+          //           ),
+          //           const Column(
+          //             children: [
+          //               Text("total"),
+          //               Text(
+          //                 "50%",
+          //                 style: TextStyle(
+          //                     fontSize: 18, fontWeight: FontWeight.w600),
+          //               )
+          //             ],
+          //           ),
+          //         ],
+          //       )
+          //     ],
+          //   ),
+          // )
         ],
       ),
     ),
   ),
-));
+);
 
 class GDPData {
   GDPData(this.continent, this.gdp);
